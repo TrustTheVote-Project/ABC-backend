3526db77a4ef58af57f123cd3afa066deab82148
Christopher <cwisdo@gnaritas.com>
2021-10-26 12:34:50 -0400

    Split latest from versioned db init

--- dev-db-init.sh
+++ dev-db-init.sh
@@ -1,10 +1,6 @@
 export AWS_PROFILE=abcbackenddeploy
 
-
 aws dynamodb batch-write-item --request-items file://db-json/populate-election-table-dev.json --output json --no-paginate
 aws dynamodb batch-write-item --request-items file://db-json/populate-voter-dev-1.json --output json --no-paginate
 aws dynamodb batch-write-item --request-items file://db-json/populate-voter-dev-2.json --output json --no-paginate
 
-aws dynamodb batch-write-item --request-items file://db-json/populate-election-table-latest.json --output json --no-paginate
-aws dynamodb batch-write-item --request-items file://db-json/populate-voter-latest-1.json --output json --no-paginate
-aws dynamodb batch-write-item --request-items file://db-json/populate-voter-latest-2.json --output json --no-paginate
