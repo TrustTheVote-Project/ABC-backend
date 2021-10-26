1d43ca57c2622107cfc668af2671901cc96decc4
Christopher <cwisdo@gnaritas.com>
2021-10-26 11:56:06 -0400

    Update data; file scheme

--- dev-db-init.sh
+++ dev-db-init.sh
@@ -2,9 +2,9 @@ export AWS_PROFILE=abcbackenddeploy
 
 
 aws dynamodb batch-write-item --request-items file://db-json/populate-election-table-dev.json --output json --no-paginate
-aws dynamodb batch-write-item --request-items file://db-json/populate-voter-table-1-dev.json --output json --no-paginate
-aws dynamodb batch-write-item --request-items file://db-json/populate-voter-table-2-dev.json --output json --no-paginate
+aws dynamodb batch-write-item --request-items file://db-json/populate-voter-dev-1.json --output json --no-paginate
+aws dynamodb batch-write-item --request-items file://db-json/populate-voter-dev-2.json --output json --no-paginate
 
 aws dynamodb batch-write-item --request-items file://db-json/populate-election-table-latest.json --output json --no-paginate
-aws dynamodb batch-write-item --request-items file://db-json/populate-voter-table-1-latest.json --output json --no-paginate
-aws dynamodb batch-write-item --request-items file://db-json/populate-voter-table-2-latest.json --output json --no-paginate
+aws dynamodb batch-write-item --request-items file://db-json/populate-voter-latest-1.json --output json --no-paginate
+aws dynamodb batch-write-item --request-items file://db-json/populate-voter-latest-2.json --output json --no-paginate
