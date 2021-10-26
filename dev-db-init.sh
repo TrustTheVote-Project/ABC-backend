8ba0387eeeb4a53817f95ad3e9f21ec8be713aa9
Christopher <cwisdo@gnaritas.com>
2021-10-25 21:54:31 -0400

    Update Latest DB with current db update

--- dev-db-init.sh
+++ dev-db-init.sh
@@ -4,3 +4,7 @@ export AWS_PROFILE=abcbackenddeploy
 aws dynamodb batch-write-item --request-items file://db-json/populate-election-table-dev.json --output json --no-paginate
 aws dynamodb batch-write-item --request-items file://db-json/populate-voter-table-1-dev.json --output json --no-paginate
 aws dynamodb batch-write-item --request-items file://db-json/populate-voter-table-2-dev.json --output json --no-paginate
+
+aws dynamodb batch-write-item --request-items file://db-json/populate-election-table-latest.json --output json --no-paginate
+aws dynamodb batch-write-item --request-items file://db-json/populate-voter-table-1-latest.json --output json --no-paginate
+aws dynamodb batch-write-item --request-items file://db-json/populate-voter-table-2-latest.json --output json --no-paginate
