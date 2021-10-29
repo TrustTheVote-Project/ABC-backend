9bd0dd5662e2f9e42f54e0d72a335e16a10381db
Christopher <cwisdo@gnaritas.com>
2021-10-29 17:56:29 -0400

    Allow targeted getElection call

--- endpoints/getElection/app.js
+++ endpoints/getElection/app.js
@@ -1,13 +1,24 @@
 const { Election, ApiResponse } = require("/opt/Common");
 
 exports.lambdaHandler = async (event, context, callback) => {
-  const elections = await Election.all();
-  if (!elections || elections.length == 0) {
-    return ApiResponse.noElectionResponse();
-  }
+  messageBody = event.body ? JSON.parse(event.body) : {};
+  if (messageBody["electionId"]) {
+    const { electionId } = messageBody;
+    const election = await Election.findByElectionId(electionId);
+    if (!election) {
+      return ApiResponse.noMatchingElection(electionId);
+    } else {
+      return ApiResponse.makeResponse(200, election.attributes);
+    }
+  } else {
+    const elections = await Election.all();
+    if (!elections || elections.length == 0) {
+      return ApiResponse.noElectionResponse();
+    }
 
-  return ApiResponse.makeResponse(
-    200,
-    elections.map((election) => election.attributes)
-  );
+    return ApiResponse.makeResponse(
+      200,
+      elections.map((election) => election.attributes)
+    );
+  }
 };
