2a17a0172cb02464fb916183c8a1c110497aec24
Christopher <cwisdo@gnaritas.com>
2022-03-12 11:13:34 -0500

    Updated voter set

--- db-init.sh
+++ db-init.sh
@@ -10,6 +10,7 @@ aws dynamodb batch-write-item --request-items file://db-json/populate-election-t
 
 aws dynamodb batch-write-item --request-items file://db-json/populate-voter-1.json --endpoint-url http://localhost:8000  --output json --no-paginate
 aws dynamodb batch-write-item --request-items file://db-json/populate-voter-2.json --endpoint-url http://localhost:8000  --output json --no-paginate
+aws dynamodb batch-write-item --request-items file://db-json/populate-voter-3.json --endpoint-url http://localhost:8000  --output json --no-paginate
 
 aws dynamodb delete-table --table-name abc_voter_sessions_local --endpoint-url http://localhost:8000  --output json --no-paginate
 aws dynamodb create-table --cli-input-json file://db-json/create-voter-sessions-table.json --endpoint-url http://localhost:8000  --output json --no-paginate
