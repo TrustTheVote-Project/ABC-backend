f5009ce54e8c044923921d7fdd24c775d2a4b877
Alexander Mekelburg <alexmek@gmail.com>
2022-12-08 09:34:42 -0500

    console log and upadte test count

--- endpoints/openElectionTest/app.js
+++ endpoints/openElectionTest/app.js
@@ -44,9 +44,7 @@ exports.lambdaHandler = async (event, context, callback) => {
         );
       }
       if (election.attributes.servingStatus == Election.servingStatus.closed) {
-        const testCount = election.attributes.testCount
-          ? 1 + election.attributes.testCount
-          : 1;
+        const testCount = (election.attributes.testCount || 0) + 1         
 
         await election.update({
           servingStatus: Election.servingStatus.open,
