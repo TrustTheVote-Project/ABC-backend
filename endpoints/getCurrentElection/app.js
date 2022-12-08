b3d223990a6051be04d917e533447cc267425ab1
Alexander Mekelburg <alexmek@gmail.com>
2022-12-08 10:11:10 -0500

    refactor user-agent checking

--- endpoints/getCurrentElection/app.js
+++ endpoints/getCurrentElection/app.js
@@ -1,9 +1,9 @@
 const { Election, ApiResponse } = require("/opt/Common");
 const { getLatModeFromEvent } = require("/opt/LatMode");
-
+  
 exports.lambdaHandler = async (event, context, callback) => {
   const latMode = getLatModeFromEvent(event);
-  
+
   const election = await Election.currentElection(latMode);
 
   if (!election) {
