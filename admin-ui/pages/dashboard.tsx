6efabcad1dc053b2885e54972bb9f0031c57ee54
Christopher <cwisdo@gnaritas.com>
2023-01-16 09:52:19 -0500

    Admin validations initial checkin

--- admin-ui/pages/dashboard.tsx
+++ admin-ui/pages/dashboard.tsx
@@ -1,32 +1,42 @@
-import type { NextPage } from 'next'
-import LoggedInLayout from 'layout/LoggedInLayout'
-import { Button, Card, Grid, Paper, TextField, Typography } from '@mui/material'
-import { useContext, useEffect, useState } from 'react'
+import type { NextPage } from "next";
+import LoggedInLayout from "layout/LoggedInLayout";
+import {
+  Button,
+  Card,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useContext, useEffect, useState } from "react";
 
-import AddIcon from '@mui/icons-material/Add';
-import theme from 'theme'
-import UserContext from 'context/UserContext'
-import { requestLoginCode } from 'requests/auth'
-import { useRouter } from 'next/router'
-import { Election, ElectionServingStatus, ElectionStatus } from 'types'
-import { getAll as getAllElections, getCurrentElection } from 'requests/election'
-import Section from 'component/Section'
-import ElectionCard from 'component/ElectionCard'
-import useElections from 'hooks/useElections'
-import GC from 'component/GC'
-import GI from 'component/GI'
-import Loading from 'component/Loading';
-import useCurrentElection from 'hooks/useCurrentElection';
+import AddIcon from "@mui/icons-material/Add";
+import theme from "theme";
+import UserContext from "context/UserContext";
+import { requestLoginCode } from "requests/auth";
+import { useRouter } from "next/router";
+import { Election, ElectionServingStatus, ElectionStatus } from "types";
+import {
+  getAll as getAllElections,
+  getCurrentElection,
+} from "requests/election";
+import Section from "component/Section";
+import ElectionCard from "component/ElectionCard";
+import useElections from "hooks/useElections";
+import GC from "component/GC";
+import GI from "component/GI";
+import Loading from "component/Loading";
+import useCurrentElection from "hooks/useCurrentElection";
 
 const Dashboard: NextPage = () => {
   const router = useRouter();
   const [elections, reloadElections, loadingElections] = useElections();
   const [currentElection, reloadCurrentElection] = useCurrentElection();
-  
-  const onUpdateElection = () => { 
+
+  const onUpdateElection = () => {
     reloadCurrentElection();
     reloadElections();
-  }
+  };
 
   let openElections: Array<Election> = [];
   let testElections: Array<Election> = [];
@@ -34,7 +44,7 @@ const Dashboard: NextPage = () => {
   let closedElections: Array<Election> = [];
   elections.forEach((e) => {
     if (e.electionStatus === ElectionStatus.live) {
-      openElections.push(e)
+      openElections.push(e);
     } else if (e.electionStatus === ElectionStatus.test) {
       testElections.push(e);
     } else if (e.electionStatus === ElectionStatus.pending) {
@@ -42,64 +52,132 @@ const Dashboard: NextPage = () => {
     } else if (e.electionStatus === ElectionStatus.complete) {
       closedElections.push(e);
     }
-  })
+  });
 
-  return <LoggedInLayout title="Dashboard">
-    <Typography variant="h1">Dashboard</Typography>
-    {loadingElections && <Loading />}
-    {!loadingElections && <Section>
-      <Typography variant="h2">Current Election</Typography>
-      {currentElection && <ElectionCard onUpdateElection={onUpdateElection} election={currentElection} currentElection={currentElection} key={currentElection.electionId} />}
-      {(!currentElection) && <Typography variant="h3">No current election.</Typography>}
-    </Section>}
-    {!loadingElections && <Section>
-      <Typography variant="h2">Open Elections</Typography>
-      {openElections.length === 0 && <Typography variant='h3'>No Open Election</Typography>}
-      <GC direction="column" spacing={2}>
-        {openElections.map((election) => {
-          return <GI  key={election.electionId}><ElectionCard onUpdateElection={onUpdateElection} currentElection={currentElection} election={election} /></GI>
-        })}
-      </GC>
-    </Section>}
-    {!loadingElections && <Section>
-      <Typography variant="h2">Test Elections</Typography>
-      {testElections.length === 0 && <Typography variant='h3'>No Test Election</Typography>}
-      <GC direction="column" spacing={2}>
-        {testElections.map((election) => {
-          return <GI  key={election.electionId}><ElectionCard onUpdateElection={onUpdateElection} currentElection={currentElection} election={election} /></GI>
-        })}
-      </GC>
-    </Section>}
-    {!loadingElections && <Section>
-      <Typography variant="h2">Draft Elections</Typography>
-      <GC direction="column" spacing={2}>
-        {pendingElections.map((election)=>{
-          return <GI  key={election.electionId}><ElectionCard onUpdateElection={onUpdateElection} currentElection={currentElection} election={election} /></GI>
-        })}
-        {pendingElections.length === 0 && <GI><Typography variant="h3">No Draft Elections</Typography></GI>}
-        <GI>
-          <GC>
+  return (
+    <LoggedInLayout title="Dashboard">
+      <Typography variant="h1">Dashboard</Typography>
+      {loadingElections && <Loading />}
+      {!loadingElections && (
+        <Section>
+          <Typography variant="h2">Current Election</Typography>
+          {currentElection && (
+            <ElectionCard
+              onUpdateElection={onUpdateElection}
+              election={currentElection}
+              currentElection={currentElection}
+              key={currentElection.electionId}
+            />
+          )}
+          {!currentElection && (
+            <Typography variant="h3">No current election.</Typography>
+          )}
+        </Section>
+      )}
+      {!loadingElections && (
+        <Section>
+          <Typography variant="h2">Open Elections</Typography>
+          {openElections.length === 0 && (
+            <Typography variant="h3">No Open Election</Typography>
+          )}
+          <GC direction="column" spacing={2}>
+            {openElections.map((election) => {
+              return (
+                <GI key={election.electionId}>
+                  <ElectionCard
+                    onUpdateElection={onUpdateElection}
+                    currentElection={currentElection}
+                    election={election}
+                  />
+                </GI>
+              );
+            })}
+          </GC>
+        </Section>
+      )}
+      {!loadingElections && (
+        <Section>
+          <Typography variant="h2">Test Elections</Typography>
+          {testElections.length === 0 && (
+            <Typography variant="h3">No Test Election</Typography>
+          )}
+          <GC direction="column" spacing={2}>
+            {testElections.map((election) => {
+              return (
+                <GI key={election.electionId}>
+                  <ElectionCard
+                    onUpdateElection={onUpdateElection}
+                    currentElection={currentElection}
+                    election={election}
+                  />
+                </GI>
+              );
+            })}
+          </GC>
+        </Section>
+      )}
+      {!loadingElections && (
+        <Section>
+          <Typography variant="h2">Draft Elections</Typography>
+          <GC direction="column" spacing={2}>
+            {pendingElections.map((election) => {
+              return (
+                <GI key={election.electionId}>
+                  <ElectionCard
+                    onUpdateElection={onUpdateElection}
+                    currentElection={currentElection}
+                    election={election}
+                  />
+                </GI>
+              );
+            })}
+            {pendingElections.length === 0 && (
+              <GI>
+                <Typography variant="h3">No Draft Elections</Typography>
+              </GI>
+            )}
             <GI>
-              <Button endIcon={<AddIcon />} onClick={()=>{
-                router.push('/elections/new')
-              }}>Create New Election</Button>
+              <GC>
+                <GI>
+                  <Button
+                    endIcon={<AddIcon />}
+                    onClick={() => {
+                      router.push("/elections/new");
+                    }}
+                  >
+                    Create New Election
+                  </Button>
+                </GI>
+              </GC>
             </GI>
           </GC>
-        </GI>
-      </GC>
-    </Section>}
-    {!loadingElections && <Section>
-      <Typography variant="h2">Completed Elections</Typography>
-      <GC direction="column" spacing={2}>
-        {closedElections.map((election)=>{
-          return <GI  key={election.electionId}><ElectionCard onUpdateElection={onUpdateElection} currentElection={currentElection} election={election} /></GI>
-        })}
-        {closedElections.length === 0 && <GI><Typography variant="h3">No Closed Elections</Typography></GI>}
-        
-      </GC>
-    </Section>}
-  </LoggedInLayout>
-}
-
+        </Section>
+      )}
+      {!loadingElections && (
+        <Section>
+          <Typography variant="h2">Completed Elections</Typography>
+          <GC direction="column" spacing={2}>
+            {closedElections.map((election) => {
+              return (
+                <GI key={election.electionId}>
+                  <ElectionCard
+                    onUpdateElection={onUpdateElection}
+                    currentElection={currentElection}
+                    election={election}
+                  />
+                </GI>
+              );
+            })}
+            {closedElections.length === 0 && (
+              <GI>
+                <Typography variant="h3">No Closed Elections</Typography>
+              </GI>
+            )}
+          </GC>
+        </Section>
+      )}
+    </LoggedInLayout>
+  );
+};
 
 export default Dashboard;
--- workers/processProvisionedUpload/app.js
+++ workers/processProvisionedUpload/app.js
@@ -1,10 +1,8 @@
-const { unzip } = require("/opt/Common");
-
-
+const { processUpload } = require("/opt/Common");
 
 exports.lambdaHandler = async (event, context, callback) => {
   console.log("Received event:", JSON.stringify(event, null, 2));
-  
+
   // Get the object from the event
   const Bucket = event.Records[0].s3.bucket.name;
   const Key = decodeURIComponent(
@@ -12,16 +10,7 @@ exports.lambdaHandler = async (event, context, callback) => {
   );
 
   try {
-    if (!Key.endsWith(".zip")) {
-      return {
-        status: 200,
-        response: "OK",
-      };
-    }
-
-
-    const result = await unzip(Bucket, Key);
-    //await DocumentInterface.documentFileUnzippingComplete(Key);
+    const result = await processUpload(Bucket, Key);
 
     return {
       status: result && 200,
