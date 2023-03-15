737068d5070b3c20ea1cc53b16fe787b8e763ee0
Renu <renu@Renus-MacBook-Pro.local>
2023-03-15 19:13:23 +0530

    Add confirmation dialog when replacing a current Election

--- admin-ui/src/component/ElectionCard.tsx
+++ admin-ui/src/component/ElectionCard.tsx
@@ -183,9 +183,13 @@ export default function ElectionCard({
                           currentElection?.electionStatus == ElectionStatus.open
                         }
                         onClick={async () => {
-                          await setCurrentElection(election.electionId);
-                          if (onUpdateElection) {
-                            onUpdateElection();
+                          if (currentElection) {
+                            router.push(`/elections/${election.electionId}/set-current`);
+                          } else {
+                            await setCurrentElection(election.electionId);
+                            if (onUpdateElection) {
+                              onUpdateElection();
+                            }
                           }
                         }}
                       >
